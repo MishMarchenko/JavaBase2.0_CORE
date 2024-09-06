0d79d553040c818b7dcb1f4552722e477551653d
MishMarchenko <mishmar4enko@gmail.com>
2024-09-06 13:14:38 +0300

    not full

---
+++ src/Collections/LinkedListEx.java
@@ -0,0 +1,42 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListEx {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+//        List<String> st = new LinkedList<>();
+//        CustomLinkedList<Integer> array = new CustomLinkedList<>();
+//        array.add(5);
+//        array.add(32);
+//        array.add(96);
+//        array.add(956);
+//        array.add(196);
+//        System.out.println(array.getSize());
+//        System.out.println(array.get(0));
+//        System.out.println(array);
+//        array.delete(4);
+//        for (int i = 0; i < array.getSize(); i++){
+//            System.out.println(array.get(i));
+//        }
+        List<Integer> linked = new LinkedList<>();
+        List<Integer> array = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++){
+            linked.add(i);
+            array.add(i);
+        }
+
+        long before = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++){
+            linked.get(i);
+        }
+        long after = System.currentTimeMillis();
+
+        System.out.println(after - before);
+//        лекция на 1.30 джава, фронт 45мин(до перерыва)
+
+    }
+}
